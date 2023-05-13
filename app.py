d31c1f9be79d40a332149bf7c474a0f8bd002bcd
coding-geek1711 <mahestpm@gmail.com>
2023-05-13 09:09:14 +0530

    Flask app

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Hello There!!"
+
+
+@app.route('/home')
+def hello():
+    return "Home api"
+
+@app.route('/bye')
+def bye():
+    return "Bye api"
