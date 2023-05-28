f903e74374deb13ddc51edb06863e7325f1a22e1
coding-geek1711 <mahestpm@gmail.com>
2023-05-29 02:13:22 +0530

    Web Page

--- app.py
+++ app.py
@@ -1,24 +1,37 @@
-from flask import Flask
+from flask import Flask, Response, render_template
 from flask import request, jsonify
 
 app = Flask(__name__)
+stringVal = ""
+prevStringVal = ""
 
 
 @app.route('/')
 def hello():
+    global stringVal
     print(request.args.get('energykey'))
+    stringVal = request.args.get('energykey')
     return "Hello There!!"
 
 @app.route('/api', methods=['POST'])
 def api():
-
     data = request.get_json()
     return data
+
+@app.route('/content', methods=['GET'])
+def content():
+    def inner():
+        global stringVal, prevStringVal
+        if stringVal != prevStringVal:
+            prevStringVal = stringVal
+            yield stringVal + '<br/>\n'
+    return Response(inner(), mimetype='text/html')
+
 
 
 @app.route('/home')
 def home():
-    return "Home api"
+    return render_template('index.html.jinja')
 
 @app.route('/bye')
 def bye():
